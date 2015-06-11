29df5c75a9cb569a319170f1f8a671698d7797fe
nickooms <oomsni@gmail.com>
2015-06-11 13:59:07 +0200

    canvas-tools try 3

--- canvas/canvas-tools.js
+++ canvas/canvas-tools.js
@@ -1,8 +1,9 @@
 var Canvas = required('canvas'),
 	Image = Canvas.Image,
 	IMG = required('./img');
-Canvas.prototype.load = function (fileName, callback) {
-	IMG.loadPNG(fileName, function (result) {
+
+Canvas.prototype.load = function(fileName, callback) {
+	IMG.loadPNG(fileName, function(result) {
 		callback(result);
-	}.bind(canvas));
+	}.bind(this));
 };
