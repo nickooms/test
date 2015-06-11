430b4fd84772f623dc9ccfa80524c1782e1fcc52
nickooms <oomsni@gmail.com>
2015-06-11 13:55:50 +0200

    Second version of canvas-tools	²

---
+++ canvas/canvas-tools.js
@@ -0,0 +1,8 @@
+var Canvas = required('canvas'),
+	Image = Canvas.Image,
+	IMG = required('./img');
+Canvas.prototype.load = function (fileName, callback) {
+	IMG.loadPNG(fileName, function (result) {
+		callback(result);
+	}.bind(canvas));
+};
