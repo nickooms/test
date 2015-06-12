9c9292a63ec756c1c50d37734a68e33e6266c2e8
Nick Ooms <oomsni@gmail.com>
2015-06-12 11:56:03 +0200

    disabled colors

--- canvas/racing.js
+++ canvas/racing.js
@@ -1,50 +1,50 @@
 var Canvas = require('canvas'),
-	canvasTools = require('./canvas-tools'),
-	alert = require('./alert'),
-	Image = Canvas.Image,
-	http = require('http'),
-	fs = require('fs'),
-	request = require('request'),
-	download = require('./download'),
-	IMG = require('./img'),
-	BBOX = require('./bbox'),
-	WMS = require('./wms');
-	floodFill = require('./floodfill'),
-	clc = require('cli-color'),
-	Color = require('./color');
+  canvasTools = require('./canvas-tools'),
+  alert = require('./alert'),
+  Image = Canvas.Image,
+  http = require('http'),
+  fs = require('fs'),
+  request = require('request'),
+  download = require('./download'),
+  IMG = require('./img'),
+  BBOX = require('./bbox'),
+  WMS = require('./wms');
+  floodFill = require('./floodfill'),
+  clc = require('cli-color'),
+  Color = require('./color');
 
 var FILE_NAME = 'wms.png';
 //console.log(clc.erase.screen);
-alert(clc.reset + clc.bold.white('             Converting ') + clc.underline.bold(FILE_NAME));
+//alert(clc.reset + clc.bold.white('             Converting ') + clc.underline.bold(FILE_NAME));
 var line = '';
 for (var i = 0; i < 0xff; i++) {
-	var msg = clc.black.bgXterm(i);
-	line += msg('       ');
+  var msg = clc.black.bgXterm(i);
+  line += msg(' ');
 }
-console.log(line);
+//console.log(line);
 if (process.argv.length > 2) {
-	var canvas = new Canvas();
-	IMG.loadPNG(__dirname + '/' + FILE_NAME, canvas, function (fileName) {
-		alert('Loaded ' + fileName);
-		alert(IMG.findColors(canvas));
-		IMG.fillAllPixels(canvas, '#cccccc', 0xff0000ff);
-		IMG.fillAllPixels(canvas, '#b7b7b7', 0x00ff00ff);
-		IMG.savePNG(__dirname + '/wms-1.png', canvas, function(fileName) {
-			console.log('Saved ' + fileName);
-		});
-	});
+  var canvas = new Canvas();
+  IMG.loadPNG(__dirname + '/' + FILE_NAME, canvas, function(fileName) {
+    alert('Loaded ' + fileName);
+    var colors = IMG.findColors(canvas);
+    IMG.fillAllPixels(canvas, '#cccccc', 0xff0000ff);
+    IMG.fillAllPixels(canvas, '#b7b7b7', 0x00ff00ff);
+    IMG.savePNG(__dirname + '/wms-1.png', canvas, function(fileName) {
+      console.log('Saved ' + fileName);
+    });
+  });
 } else {
-	var bbox = new BBOX(152512, 221815, 152688, 221918);
-	var query = WMS.query({
-		layers: [
-			WMS.WBN,
-	//    WMS.WVB,
-	//    WMS.WGO
-		],
-		bbox: bbox,
-		size: 1000
-	});
-	download(query, __dirname + '/wms.png', function() {
-		console.log('done');
-	});
+  var bbox = new BBOX(152512, 221815, 152688, 221918);
+  var query = WMS.query({
+    layers: [
+      WMS.WBN,
+      //    WMS.WVB,
+      //    WMS.WGO
+    ],
+    bbox: bbox,
+    size: 1000
+  });
+  download(query, __dirname + '/wms.png', function() {
+    console.log('done');
+  });
 }
