addc473132711c9105288ada23c0ce0483cfb929
Nick Ooms <oomsni@gmail.com>
2015-06-15 06:40:19 +0200

    Refactoring

--- canvas/color.js
+++ canvas/color.js
@@ -1,5 +1,4 @@
 var fs = require('fs'),
-<<<<<<< HEAD
 	xml2js = require('xml2js'),
 	alert = require('./alert'),
 	RGB = require('./rgb'),
@@ -9,69 +8,7 @@ var Color = function() {
 };
 Color.colors = [];
 
-/*var color, parser = new xml2js.Parser();
-=======
-	xml2js = require('xml2js');
-
-var color, parser = new xml2js.Parser();
->>>>>>> efa30ad6b3dd8dee4eee772809ae4777ff7f11bd
-fs.readFile(__dirname + '/color-table.html', function(err, data) {
-	parser.parseString(data, function (err, result) {
-		var rows = result.table.tr;
-		var colors = [];
-		for (var i = 0; i < rows.length; i++) {
-			var cells = rows[i].td;
-			for (var j = 0; j < cells.length; j++) {
-				var cell = cells[j];
-				if (typeof cell == 'string') {
-					color = { nr: parseInt(cell) };
-				} else {
-<<<<<<< HEAD
-					color.code = ('#' + cell.img[0].$.src.split('/').reverse()[0].split('.')[0]).toUpperCase();
-					Color.colors.push(color);
-				}
-			}
-		}
-		console.log('Done parsing terminal colors');
-	});
-});
-Color.rgb = function(color) {
-	return {
-		r: parseInt(color.substr(1, 2), 16),
-		g: parseInt(color.substr(3, 2), 16),
-		b: parseInt(color.substr(5, 2), 16)
-	};
-};
-Color.find = function(color) {
-	var rgb = new RGB(color);
-	var colors = Color.colors.map(function(color) {
-		var c = new RGB(color.code);
-		return {
-			nr: color.nr,
-			difference: new RGBDifference(c, rgb)
-		};
-	});
-	colors.sort(function(a, b) {
-		return a.difference.toInt() > b.difference.toInt() ? 1 : -1;
-	});
-	return colors[0].nr;
-};*/
 Color.load = function() {
-=======
-					//console.log(cell.img[0].$.src.split('/').reverse()[0].split('.')[0]);
-					colors.push(color);
-				}
-			}
-		}
-		//console.dir(result);
-		//console.log('Done');
-	});
-});
 
-var Color = function() {
-};
-Color.load = function() {
-//	fs.read
->>>>>>> efa30ad6b3dd8dee4eee772809ae4777ff7f11bd
 }
 module.exports = Color;
--- canvas/racing.js
+++ canvas/racing.js
@@ -1,5 +1,4 @@
 var Canvas = require('canvas'),
-<<<<<<< HEAD
 	canvasTools = require('./canvas-tools'),
 	alert = require('./alert'),
 	Image = Canvas.Image,
@@ -132,54 +131,4 @@ if (process.argv.length > 2) {
 	download(query, __dirname + '/streets/' + bbox.toString() + '.png', function() {
 		console.log('done');
 	});
-=======
-  canvasTools = require('./canvas-tools'),
-  alert = require('./alert'),
-  Image = Canvas.Image,
-  http = require('http'),
-  fs = require('fs'),
-  request = require('request'),
-  download = require('./download'),
-  IMG = require('./img'),
-  BBOX = require('./bbox'),
-  WMS = require('./wms');
-  floodFill = require('./floodfill'),
-  clc = require('cli-color'),
-  Color = require('./color');
-
-var FILE_NAME = 'wms.png';
-process.stdout.write(clc.reset);
-//alert(clc.reset + clc.bold.white('             Converting ') + clc.underline.bold(FILE_NAME));
-var line = '';
-for (var i = 0; i < 0xff; i++) {
-  var msg = clc.black.bgXterm(i);
-  line += msg(' ');
-}
-//console.log(line);
-if (process.argv.length > 2) {
-  var canvas = new Canvas();
-  IMG.loadPNG(__dirname + '/' + FILE_NAME, canvas, function(fileName) {
-    alert('Loaded ' + fileName);
-    var colors = IMG.findColors(canvas);
-    IMG.fillAllPixels(canvas, '#cccccc', 0xff0000ff);
-    IMG.fillAllPixels(canvas, '#b7b7b7', 0x00ff00ff);
-    IMG.savePNG(__dirname + '/wms-1.png', canvas, function(fileName) {
-      console.log('Saved ' + fileName);
-    });
-  });
-} else {
-  var bbox = new BBOX(152512, 221815, 152688, 221918);
-  var query = WMS.query({
-    layers: [
-      WMS.WBN,
-      //    WMS.WVB,
-      //    WMS.WGO
-    ],
-    bbox: bbox,
-    size: 1000
-  });
-  download(query, __dirname + '/wms.png', function() {
-    console.log('done');
-  });
->>>>>>> efa30ad6b3dd8dee4eee772809ae4777ff7f11bd
 }
